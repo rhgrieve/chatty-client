0d9fc6f372b0c58a6d6fd2af60ec6aa854103445
Harrison Grieve <hgrieve@canadagoose.com>
2022-03-19 14:27:06 +0100

    feat: ui enhancements
    Add markdown support, emoji support, enter to send message and scrolling

---
+++ src/static/helpers.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export interface MessageData {
+  message: string;
+  username: string;
+  datetime: string;
+  color: string;
+  statusMessage?: string;
+}
+
+export class User {
+  uuid: string;
+
+  username: string;
+
+  connection: WebSocket;
+
+  constructor(username: string) {
+    this.username = username;
+    this.uuid = uuidv4();
+  }
+
+  connect(socket: WebSocket) {
+    this.connection = socket;
+    this.send('', 'connect');
+  }
+
+  disconnect() {
+    this.send('', 'disconnect');
+    this.connection.close();
+  }
+
+  send(message: string, statusMessage?: string) {
+    this.connection.send(JSON.stringify({
+      id: this.uuid,
+      username: this.username,
+      datetime: new Date().toLocaleTimeString(),
+      statusMessage,
+      message,
+    }));
+  }
+}
